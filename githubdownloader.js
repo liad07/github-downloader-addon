c0d34465dd04dde16f84e0543ae2458e94df84e9
liadk07 <91607695+liad07@users.noreply.github.com>
2022-06-20 18:25:36 +0300

    Add files via upload

---
+++ githubdownloader.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("btn-primary btn")[2].addEventListener("click", f);
+document.getElementsByClassName("btn-primary btn")[2].textContent="download";
+function f(){
+    window.open(document.getElementsByClassName("d-flex flex-items-center color-fg-default text-bold no-underline") [2].href)
+}
